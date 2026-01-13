9cca597a6951a02c19d16cdc8162247138455ab6
Siva <siva@i27academy.com>
2026-01-13 07:35:29 +0530

    added auth method

--- src/com/i27academy/k8s/K8s.groovy
+++ src/com/i27academy/k8s/K8s.groovy
@@ -1,5 +1,23 @@
 // have the groovy implementation, a shared lib for our complte ui and apis
-// package com.i27academy.k8s
-// class K8s {
-//     def jenkins
-// }
+package com.i27academy.k8s
+class K8s {
+    def jenkins
+
+    K8s(jenkins) {
+        this.jenkins = jenkins
+    }
+
+    // method to connect to gke clusters
+    def auth_login() {
+        jenkins.sh """
+            echo "******************* Authenticating to K8S Cluster ***********"
+            gcloud container clusters get-credentials i27-cluster --zone us-central1-a --project proven-wavelet-481608-k1
+            kubectl get nodes
+        """
+    }
+
+    // method to connect to eks clusters
+}
+
+
+//gcloud container clusters get-credentials i27-cluster --zone us-central1-a --project proven-wavelet-481608-k1
